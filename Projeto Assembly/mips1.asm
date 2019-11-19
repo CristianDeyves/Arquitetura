266b75bca7074ac4bebbd17f9fd448d9dad2dbe6
Cristian Deyves Oliveira de Brito <deyves@A204-02L.imd.local>
2019-11-19 14:52:33 -0300

    Organizando

--- Projeto Assembly/mips1.asm
+++ Projeto Assembly/mips1.asm
@@ -57,70 +57,13 @@
 .text
 	.globl main
 	main:
-		# Contador do loopBombas
-		move $s3, $zero
-		
-		#passo 1 colocar bombas na matriz
+		#passo 1 colocar bombas na matriz total de 40 bombas
 			#passo 1.2 verificar quantidade total de bombas
 		
+		# Contador de rand
+		move $t1, $zero
 		
-		
-		#passo 2 imprimir matriz de caracteres
-		
-		#passo 3 solicitar linha e coluna do usuario
-		
-		#passo 4 verificar se tem bomba na posição e verificar os vizinhos
-		
-		#passo 5 colocar as dicas
-		
-		#passo 6 verifica se o usuario ganhou
-		
-		#passo 7 caso o usuario nao ganhou voltar ao passo 2
-		
-		
-		# carregar endereco do mdArray[0][0]
-		la $a0, mdArray
-		# carregar o tamanho da matriz (numero de linhas e colunas)
-		lw $a1, size
-		# funcao de somar a diagonal
-		jal sumDiagonal
-		
-		# imprimir o resultado da funcao
-		move $a0, $v0
-		li $v0, 1
-		syscall 
-		
-		#j randomNumber
-		j printMatriz
-		
-		#end
-		li $v0, 10
-		syscall
-		
-	sumDiagonal:
-		li $v0, 0 # soma = 0
-		li $t0, 0 # index
-		
-		sumLoop:
-			mul $t1,$t0,$a1 # t1 = index * colSize
-			add $t1,$t1,$t0 #                 + colIndex
-			mul $t1, $t1, DATA_SIZE # * data_size
-			add $t1, $t1, $a0   # + base address
-			
-			lw $t2, ($t1)
-			add $v0, $v0, $t2  # sum = sum + mdArray[i][i]
-			
-			
-			addi $t0, $t0, 1   # i++
-			blt $t0, $a1, sumLoop  # if i < size, loop again
-			
-		jr $ra
-		
-		
-		
-		
-		
-	randomNumber:
+		randomNumber:
 		li $v0, 30        # Syscall 30: System Time syscall
 		syscall           # $a0 will contain the 32 LS bits of the system time
 		move $t0, $a0     # Save $a0 value in $t0 
@@ -140,79 +83,21 @@
 		li $v0, 1   # 1 is the system call code to show an int number
 		syscall     # as I said your generated number is at $a0, so it will be printed
 		
-		j loopBombas
+		# Incrementa contador
+		addi $t1,$t1,1
 		
+		# Conferir contador
+		bne $t1,1,randomNumber
 		
-		loopBombas:
-			
-			# Compara se se todas as bombas foram preenchdas.
-			
+		#passo 2 imprimir matriz de caracteres
 		
+		#passo 3 solicitar linha e coluna do usuario
 		
+		#passo 4 verificar se tem bomba na posição e verificar os vizinhos
 		
-	printMatriz:
-		# carregar endereco da matriz em t1 e em k0
-		la $t1, mdArray
-		la $k0, mdArray
+		#passo 5 colocar as dicas
 		
-		# valor limite
-		li $s6, 21
+		#passo 6 verifica se o usuario ganhou
 		
-		#contador linha
-		li $s0, 1
-		
-		# imprimir linha por linha
-		loopLinha:
-			# imprimir pular linha(\n)
-			li $v0, 4
-			la $a0, nline
-			syscall
-			# se linha = 11, terminar
-			beq $s0, $s6, endPrintMatriz
-			#contador coluna
-			li $s1, 1
-			
-			loopColuna:
-				# carregar valor do endereco em t2
-				lw $t2, 0($t1)
-				
-				# imprimir valor
-				li $v0, 1
-				move $a0, $t2
-				syscall
-				
-				# optional - syscall number for printing character (space)
-    				li      $a0, 32
-    				li      $v0, 11  
-    				syscall
-    
-				# avancar para proximo endereco
-				mul $t1, $s0, 22
-				add $t1, $t1, $s1
-				mul $t1, $t1, DATA_SIZE
-				add $t1, $t1, $k0
-				
-				# coluna++
-				addi $s1, $s1, 1
-			
-				# se coluna != 11, continuar loop
-				bne $s1, $s6, loopColuna
-			
-				# senao, linha++
-				addi $s0, $s0, 1
-				# recomecar
-				j loopLinha
-		
-		endPrintMatriz:
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		#passo 7 caso o usuario nao ganhou voltar ao passo 2
 		
